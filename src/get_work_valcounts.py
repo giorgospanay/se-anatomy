a32a34169f9f93f1a79adfd1344173289e4a13a9
Georgios Panayiotou <giorgospanay@gmail.com>
2024-12-05 10:40:23 +0100

    get vals

---
+++ src/get_work_valcounts.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+## GLOBALS
+csv_path="../results2"
+log_path="../result_logs"
+plot_path="../result_plots"
+obj_path=csv_path
+
+lisa_path="../../mat_lev_lisa_2017.csv"
+deso_path="../../mat_lev_deso_2017.csv"
+
+
+#read in data 
+data = pd.read_csv(lisa_path, usecols=["CfarNr_LISA", "Alder", "AstNr_LISA"])
+print(f"Lisa length: {len(data.index)}")
+# data_deso = pd.read_csv(deso_path)
+# print(f"Deso length: {len(data_deso.index)}")
+
+
+# get value counts for companies
+print(data["CfarNr_LISA"].value_counts())
+
+
+# get value counts for workplaces?
+
+
