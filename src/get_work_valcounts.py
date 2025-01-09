81ad1ef69d186cec977c93ce7b35816171d5e11e
Georgios Panayiotou <giorgospanay@gmail.com>
2025-01-09 12:37:41 +0200

    testing once again

--- src/get_work_valcounts.py
+++ src/get_work_valcounts.py
@@ -41,8 +41,8 @@ def convert_to_remote(value):
         return str(value)
 
 #read in data 
-data = pd.read_csv(lisa_path,index_col="LopNr",usecols=["LopNr","LopNr_CfarNr","LopNr_ArbstId","AstNr_LISA","AstKommun"])
-#data = pd.read_csv(lisa_path,index_col="LopNr",usecols=["LopNr","LopNr_PeOrgNr","LopNr_ArbstId","AstNr_LISA","AstKommun"])
+#data = pd.read_csv(lisa_path,index_col="LopNr",usecols=["LopNr","LopNr_CfarNr","LopNr_ArbstId","AstNr_LISA","AstKommun"])
+data = pd.read_csv(lisa_path,index_col="LopNr",usecols=["LopNr","LopNr_PeOrgNr","LopNr_ArbstId","AstNr_LISA","AstKommun"])
 
 print(f"Lisa length: {len(data.index)}")
 print(data.columns)
@@ -65,17 +65,19 @@ print(f"Filtered dups length: {len(data.index)}")
 # Remove filtered kommuns
 filter_data=data[~data["AstKommun"].astype(int).isin([0,9999])]
 
-filter_data=filter_data[filter_data["LopNr_CfarNr"]!="-"]
-#filter_data=filter_data[filter_data["LopNr_PeOrgNr"]!="-"]
+#filter_data=filter_data[filter_data["LopNr_CfarNr"]!="-"]
+filter_data=filter_data[filter_data["LopNr_PeOrgNr"]!="-"]
 
 
 print(filter_data[["AstNr_LISA"]].value_counts(dropna=False))
 
 
 
-gc = filter_data[["LopNr_CfarNr","AstNr_LISA"]].groupby(["LopNr_CfarNr","AstNr_LISA"],as_index=False).groups
+#gc = filter_data[["LopNr_CfarNr","AstNr_LISA"]].groupby(["LopNr_CfarNr","AstNr_LISA"],as_index=False).groups
+gc = filter_data[["LopNr_PeOrgNr","AstNr_LISA"]].groupby(["LopNr_PeOrgNr","AstNr_LISA"],as_index=False).groups
+
 for group in gc:
-	print(f"{group}: {gc[group]} (size: {len(gc[group])})")
+	print(f"{group}: {gc[group].values} (size: {len(gc[group])})")
 
 
 
@@ -84,68 +86,59 @@ print(f"Filtered Lisa length: {len(filter_data.index)}")
 filter_data["AstNr_LISA"]=filter_data["AstNr_LISA"].astype(str).apply(convert_to_remote)
 
 
-# Also remove outlier workplace
-filter_data=filter_data[filter_data["LopNr_CfarNr"].astype(int)!=946067]
-#filter_data=filter_data[filter_data["LopNr_PeOrgNr"].astype(int)!=946067]
+# # Also remove outlier workplace
+# filter_data=filter_data[filter_data["LopNr_CfarNr"].astype(int)!=946067]
+# #filter_data=filter_data[filter_data["LopNr_PeOrgNr"].astype(int)!=946067]
 
-print(f"Filtered after 946067 length: {len(filter_data.index)}")
+# print(f"Filtered after 946067 length: {len(filter_data.index)}")
 
-print(filter_data.value_counts())
+# print(filter_data.value_counts())
 
 
-# # Print users with workplace=Remote, see what they look like
-# filter_data=filter_data[filter_data["AstNr_LISA"]=="Remote"]
+# # # Print users with workplace=Remote, see what they look like
+# # filter_data=filter_data[filter_data["AstNr_LISA"]=="Remote"]
+# # print(filter_data)
+
+
+
+# # Read work attrbs per user
+# data_attb=pd.read_csv(f"{log_path}/filtered_node_final_2017.csv",index_col="PersonNr",usecols=["PersonNr","deg_work"],header=0)
+# # Merge with user
+# filter_data=filter_data.merge(data_attb,left_on="LopNr",right_on="PersonNr")
 # print(filter_data)
 
 
-
-# Read work attrbs per user
-data_attb=pd.read_csv(f"{log_path}/filtered_node_final_2017.csv",index_col="PersonNr",usecols=["PersonNr","deg_work"],header=0)
-# Merge with user
-filter_data=filter_data.merge(data_attb,left_on="LopNr",right_on="PersonNr")
-print(filter_data)
+# # # Print users with deg=0, see what they look like
+# # filter_data=filter_data[filter_data["deg_work"]==0]
+# # print(f"Filtered deg=0 Lisa length: {len(filter_data.index)}")
+# # print(filter_data)
 
 
-# # Print users with deg=0, see what they look like
-# filter_data=filter_data[filter_data["deg_work"]==0]
-# print(f"Filtered deg=0 Lisa length: {len(filter_data.index)}")
+# filter_data.sort_values("LopNr_CfarNr",inplace=True)
+
+# #print(filter_data[["LopNr_CfarNr","AstNr_LISA"]])
 # print(filter_data)
 
-
-filter_data.sort_values("LopNr_CfarNr",inplace=True)
-
-#print(filter_data[["LopNr_CfarNr","AstNr_LISA"]])
-print(filter_data)
-
-dat=filter_data[["LopNr_CfarNr","AstNr_LISA","deg_work"]].groupby(by=["LopNr_CfarNr"],dropna=False).agg(g_size=("LopNr_CfarNr","size"),avg_deg=("deg_work","mean"))
-print(dat)
+# dat=filter_data[["LopNr_CfarNr","AstNr_LISA","deg_work"]].groupby(by=["LopNr_CfarNr"],dropna=False).agg(g_size=("LopNr_CfarNr","size"),avg_deg=("deg_work","mean"))
+# print(dat)
 
 
 
-# Replace index
-group=filter_data[["LopNr_CfarNr","AstNr_LISA"]].reset_index()
-group.set_index(["LopNr_CfarNr","AstNr_LISA"],inplace=True)
+# # Replace index
+# group=filter_data[["LopNr_CfarNr","AstNr_LISA"]].reset_index()
+# group.set_index(["LopNr_CfarNr","AstNr_LISA"],inplace=True)
 
-# group=filter_data[["LopNr_PeOrgNr","AstNr_LISA"]].reset_index()
-# group.set_index(["LopNr_PeOrgNr","AstNr_LISA"],inplace=True)
-
-
-print(group)
-
-group_a=group.value_counts()
-print(group_a)
-
-# Filter
-f_ga=group_a[group_a>1]
-
-print(f_ga)
-print(f_ga.sum())
+# # group=filter_data[["LopNr_PeOrgNr","AstNr_LISA"]].reset_index()
+# # group.set_index(["LopNr_PeOrgNr","AstNr_LISA"],inplace=True)
 
 
+# print(group)
 
+# group_a=group.value_counts()
+# print(group_a)
 
+# # Filter
+# f_ga=group_a[group_a>1]
 
-
-
-
-
+# print(f_ga)
+# print(f_ga.sum())
